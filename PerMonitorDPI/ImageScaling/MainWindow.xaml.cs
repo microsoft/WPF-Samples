5c50fc3e7c52a44fd42bdbd552d51b5ca86ea8d7
Rob Relyea <rob@relyeas.net>
2015-11-03 16:00:03 -0800

    Write permission test.

--- PerMonitorDPI/ImageScaling/MainWindow.xaml.cs
+++ PerMonitorDPI/ImageScaling/MainWindow.xaml.cs
@@ -11,7 +11,6 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace ImageScaling
