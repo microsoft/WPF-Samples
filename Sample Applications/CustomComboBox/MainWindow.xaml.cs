2d95648ec978cf883012318cec7b30ab147c74d5
Mohamed Elshawaf <m_shawaf@outlook.com>
2018-02-21 22:23:17 +0200

    set FocusVisualStyle to null

--- Sample Applications/CustomComboBox/MainWindow.xaml.cs
+++ Sample Applications/CustomComboBox/MainWindow.xaml.cs
@@ -24,5 +24,9 @@ namespace CustomComboBox
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+        }
     }
 }
