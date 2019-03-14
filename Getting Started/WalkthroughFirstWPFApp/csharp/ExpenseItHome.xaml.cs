3674463db548b86f912ea3587697497884bb3eb4
William Rohm (Aquent LLC) <v-wiroh@microsoft.com>
2019-03-14 14:08:51 -0700

    initial upload

---
+++ Getting Started/WalkthroughFirstWPFApp/csharp/ExpenseItHome.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExpenseIt
+{
+    /// <summary>
+    /// Interaction logic for ExpenseItHome.xaml
+    /// </summary>
+    public partial class ExpenseItHome : Page
+    {
+        public ExpenseItHome()
+        {
+            InitializeComponent();
+        }
+
+   
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // View Expense Report
+            ExpenseReportPage expenseReportPage = new ExpenseReportPage(this.peopleListBox.SelectedItem);
+            this.NavigationService.Navigate(expenseReportPage);
+
+        }
+       
+    }
+}
---
+++ Getting Started/WalkthroughFirstWPFApp/vb/ExpenseItHome.xaml.vb
@@ -0,0 +1,22 @@
+﻿Imports System.Text
+
+Namespace ExpenseIt
+	''' <summary>
+	''' Interaction logic for ExpenseItHome.xaml
+	''' </summary>
+	Partial Public Class ExpenseItHome
+		Inherits Page
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+
+		Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			' View Expense Report
+			Dim expenseReportPage As New ExpenseReportPage(Me.peopleListBox.SelectedItem)
+			Me.NavigationService.Navigate(expenseReportPage)
+
+		End Sub
+
+	End Class
+End Namespace
