da28fadb5be3ae22dcb1acffe453e88074f3cbfa
Mohamed Elshawaf <m_shawaf@outlook.com>
2019-04-30 02:38:01 +0200

    Adding CustomComboBox project to the Sample Applications (#52)

---
+++ Sample Applications/CustomComboBox/DropList.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace CustomComboBox
+{
+    class DropList : ContentControl
+    {
+        public ICommand Command
+        {
+            get { return (ICommand)GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.Register("Command", typeof(ICommand), typeof(DropList), new PropertyMetadata(null));
+
+        public object CommandParameter
+        {
+            get { return GetValue(CommandParameterProperty); }
+            set { SetValue(CommandParameterProperty, value); }
+        }
+
+        public static readonly DependencyProperty CommandParameterProperty =
+            DependencyProperty.Register("CommandParameter", typeof(object), typeof(DropList), new PropertyMetadata(null));
+
+        public string SubTitle
+        {
+            get { return (string)GetValue(SubTitleProperty); }
+            set { SetValue(SubTitleProperty, value); }
+        }
+
+        public static readonly DependencyProperty SubTitleProperty =
+            DependencyProperty.Register("SubTitle", typeof(string), typeof(DropList));
+
+        public string DynamicText
+        {
+            get { return (string)GetValue(DynamicTextProperty); }
+            set { SetValue(DynamicTextProperty, value); }
+        }
+
+        public static readonly DependencyProperty DynamicTextProperty =
+            DependencyProperty.Register("DynamicText", typeof(string), typeof(DropList));
+
+
+        public IEnumerable<object> ItemsSource
+        {
+            get { return (IEnumerable<object>)GetValue(ItemsSourceProperty); }
+            set { SetValue(ItemsSourceProperty, value); }
+        }
+
+        public static readonly DependencyProperty ItemsSourceProperty =
+            DependencyProperty.Register("ItemsSource", typeof(IEnumerable<object>), typeof(DropList));
+
+
+        public string DisplayMemberPath
+        {
+            get { return (string)GetValue(DisplayMemberPathProperty); }
+            set { SetValue(DisplayMemberPathProperty, value); }
+        }
+
+        public static readonly DependencyProperty DisplayMemberPathProperty =
+            DependencyProperty.Register("DisplayMemberPath", typeof(string), typeof(DropList), new PropertyMetadata(null));
+
+    }
+}
---
+++ Sample Applications/CustomComboBox/MainWindowViewModel.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace CustomComboBox
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public MainWindowViewModel()
+        {
+            Movies2 = new List<string>
+            {
+                "Movie1",
+                "Movie2",
+                "Movie3",
+                "Movie4",
+                "Movie5",
+                "Movie6"
+            };
+            Movies = new List<Movie>
+            {
+                new Movie{Id=1, Title="The Dark Knight" },
+                new Movie{Id=2, Title="The Big Lebowski" },
+                new Movie{Id=3, Title="The Shawshank Redemption" },
+                new Movie{Id=4, Title="Schindler's List" },
+                new Movie{Id=5, Title="Pulp Fiction" },
+                new Movie{Id=6, Title="Fight Club" }
+            };
+
+            OnWatchNow = new WatchNowCommand(ShowMovie);
+        }
+
+        private void ShowMovie(object parameter)
+        {
+            if (parameter != null)
+            {
+
+                if (parameter is string s)
+                    SelectedMovie = s;
+                else if (parameter is Movie m)
+                    SelectedMovie = m.Title;
+            }
+        }
+
+        public List<Movie> Movies { get; set; }
+        public List<string> Movies2 { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+        public ICommand OnWatchNow { get; set; }
+        string selectedMovie;
+        public string SelectedMovie
+        {
+            get { return selectedMovie; }
+            set
+            {
+                selectedMovie = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedMovie)));
+            }
+        }
+    }
+
+    public class WatchNowCommand : ICommand
+    {
+        Action<object> _execute;
+        public WatchNowCommand(Action<object> execute)
+        {
+            _execute = execute;
+        }
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter) => true;
+
+        public void Execute(object parameter) =>
+            _execute(parameter);
+
+    }
+}
