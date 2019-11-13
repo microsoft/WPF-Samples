ee00ec6edece57384fc49911cbc0ab2d9f851b3c
Vatsan Madhavan <vatsan.madhavan@microsoft.com>
2019-11-13 15:56:41 -0800

    Use simpler scheme contained in global.json for alternate SDK versions.

--- eng/EnsureGlobalJsonSdk.ps1
+++ eng/EnsureGlobalJsonSdk.ps1
@@ -14,8 +14,42 @@ param(
 
   [string] [Alias('a')]
   [Parameter(HelpMessage='Architecture')]
-  $architecture=$env:PROCESSOR_ARCHITECTURE
+  $architecture=$env:PROCESSOR_ARCHITECTURE, 
+
+  [string] [Alias('f')]
+  [Parameter(HelpMessage='TargetFramework to match from global.json/altsdk section for an alternate SDK version')]
+  [ValidateSet('', $null, 'netcoreapp3.0', 'netcoreapp3.1', 'netcoreapp5.0', IgnoreCase=$true)]
+  $TargetFramework=''
 )
+
+Function IIf($If, $Then, $Else) {
+    If ($If -IsNot "Boolean") {$_ = $If}
+    If ($If) {If ($Then -is "ScriptBlock") {&$Then} Else {$Then}}
+    Else {If ($Else -is "ScriptBlock") {&$Else} Else {$Else}}
+}
+
+Function Get-Tfm {
+    param(
+        [string]$SdkVersion
+    )
+
+    $WellKnownTFMs = @(
+        'netcoreapp1.0',
+        'netcoreapp1.1',
+        'netcoreapp2.0',
+        'netcoreapp2.1',
+        'netcoreapp2.2',
+        'netcoreapp3.0',
+        'netcoreapp3.1',
+        'netcoreapp5.0'
+    )
+
+    $tfm = ('netcoreapp' + $SdkVersion.Substring(0,3)).Trim().ToLowerInvariant()
+
+    return IIf ($WellKnownTFMs -icontains $tfm) $tfm ""
+}
+
+
 
 # Use-RunAs function from TechNet Script Gallery
 # https://gallery.technet.microsoft.com/scriptcenter/63fd1c0d-da57-4fb4-9645-ea52fc4f1dfb
@@ -55,7 +89,29 @@ function Use-RunAs {
 
 Use-RunAs
 if (Test-Path $globalJson) {
-    $sdk_version = (Get-Content $globalJson | ConvertFrom-Json).sdk.version
+    $json = Get-Content $globalJson | ConvertFrom-Json
+
+    <# 
+        If the $TargetFramework is no different from
+        that of the version supplied in global.json/sdk.version, don't
+        use it for further decisions.
+    #>
+    if ($TargetFramework -ieq (Get-Tfm -SdkVersion $json.sdk.version)) {
+        $TargetFramework = ''
+    }
+
+    if (-not $TargetFramework) {
+        $sdk_version = $json.sdk.version
+    } else {
+        Write-Verbose "Alternate TargetFramework requested - reading from altsdk section in global.json"
+        $sdk_version = $json.altsdk.$TargetFramework
+        if ($sdk_version) {
+            Write-Verbose "Alternate SDK Version: $sdk_version"
+        } else {
+            Write-Verbose "Alternate SDK Version not found"
+        }
+    }
+
     if ($sdk_version) {
         $dotnet_install = "$env:TEMP\dotnet-install.ps1"
 
@@ -70,5 +126,17 @@ if (Test-Path $globalJson) {
         
         Write-Host "##vso[task.prependpath]$installPath"
         Write-Verbose "Added $installPath to Azure Pipelines PATH variable"
+
+        <# 
+            If $TargetFramework is specified, then an alternate SDK was requested.
+            This means that the build requires an updated global.json as well. 
+
+            This is a destructive change. The global.json should be restored by doing this after a build:
+                    git checkout global.json
+        #>
+        if ($TargetFramework) {
+            $dotnet = Join-Path $installPath 'dotnet.exe'
+            & $dotnet new globaljson --sdk-version $sdk_version --force
+        }
     }
 }
