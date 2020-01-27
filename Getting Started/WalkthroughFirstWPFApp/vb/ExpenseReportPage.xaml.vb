c2d9f24f3edd1ba79c968ac4ba0db03ac1c20e34
vartikav <vartikav@users.noreply.github.com>
2020-01-27 13:18:22 -0800

    Modification of ExpenseItIntro.vbproj (#253)
    * Modification of ExpenseItIntro.vbproj, adding it to GettingStarted.sln and WPFSamples.sln, removing GenerateAssemblyInfo settings from accessibility sample apps
    
    * removed assemblyInfo.cs files from accessibility projects
    
    * Renamed ExpenseItIntro vbproj to ExpenseItIntro2.vbproj, removed assemblyInfo.vb

--- Getting Started/WalkthroughFirstWPFApp/vb/ExpenseReportPage.xaml.vb
+++ Getting Started/WalkthroughFirstWPFApp/vb/ExpenseReportPage.xaml.vb
@@ -1,21 +1,23 @@
+Imports System.Windows.Controls
+
 Namespace ExpenseIt
-	''' <summary>
-	''' Interaction logic for ExpenseReportPage.xaml
-	''' </summary>
+    ''' <summary>
+    ''' Interaction logic for ExpenseReportPage.xaml
+    ''' </summary>
 
-	Partial Public Class ExpenseReportPage
-		Inherits Page
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+    Partial Public Class ExpenseReportPage
+        Inherits Page
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-		' Custom constructor to pass expense report data
-		Public Sub New(ByVal data As Object)
-			Me.New()
-			' Bind to expense report data.
-			Me.DataContext = data
-		End Sub
+        ' Custom constructor to pass expense report data
+        Public Sub New(ByVal data As Object)
+            Me.New()
+            ' Bind to expense report data.
+            Me.DataContext = data
+        End Sub
 
-	End Class
+    End Class
 
 End Namespace
