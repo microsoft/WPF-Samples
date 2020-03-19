4a86203b9d11de0f5f4998b8a8fba84567849fcf
Vatsan Madhavan <vatsan.madhavan@microsoft.com>
2020-03-19 15:31:12 -0700

    Some env values don't parse correctly and result in errors. fixing.

--- eng/build.ps1
+++ eng/build.ps1
@@ -236,7 +236,10 @@ Function Start-VsDevCmd {
     if ($installationPath -and (test-path "$installationPath\Common7\Tools\vsdevcmd.bat")) {
         & "${env:COMSPEC}" /s /c "`"$installationPath\Common7\Tools\vsdevcmd.bat`" -no_logo && set" | foreach-object {
             $name, $value = $_ -split '=', 2
-            set-content env:\"$name" $value
+            Write-Verbose "Setting env:$name=$value"
+            if ($name -and $value) {
+                set-content env:\"$name" "$value"
+            }
         }
     }
 }
@@ -456,4 +459,3 @@ Finally {
         Copy-Item (Join-Path $ArtifactsTemp 'NuGet.config') $NuGetConfig -Force
     }
 }
-
