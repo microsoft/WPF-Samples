16ff2831667485e5c93a812810c2e5ac91279f1b
Kadir Ince <kadir.ince@taac.com.tr>
2023-03-09 11:26:26 +0300

    CompositionTarget project layout made responsive, name convention update to PascalCase for controls

--- Visual Layer/CompositionTarget/MainWindow.cs
+++ Visual Layer/CompositionTarget/MainWindow.cs
@@ -5,6 +5,7 @@ using System;
 using System.Diagnostics;
 using System.Globalization;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 
@@ -15,9 +16,13 @@ namespace CompositionTarget
     /// </summary>
     public partial class MainWindow : Window
     {
-        private readonly Stopwatch _stopwatch = new Stopwatch();
+        private readonly Stopwatch _stopwatch = new();
         private double _frameCounter;
         private Point _pt;
+
+        private long _frameRate;
+        private byte _redColor;
+        private byte _blueColor;
 
         public MainWindow()
         {
@@ -37,25 +42,25 @@ namespace CompositionTarget
             }
 
             // Determine frame rate in fps (frames per second).
-            var frameRate = (long) (_frameCounter/_stopwatch.Elapsed.TotalSeconds);
-            if (frameRate > 0)
+            _frameRate =  (long)(_frameCounter/_stopwatch.Elapsed.TotalSeconds);
+            if (_frameRate > 0)
             {
                 // Update elapsed time, number of frames, and frame rate.
-                myStopwatchLabel.Content = _stopwatch.Elapsed.ToString();
-                myFrameCounterLabel.Content = _frameCounter.ToString(CultureInfo.InvariantCulture);
-                myFrameRateLabel.Content = frameRate.ToString();
+                MyStopwatchLabel.Content = _stopwatch.Elapsed.ToString();
+                MyFrameCounterLabel.Content = _frameCounter.ToString(CultureInfo.InvariantCulture);
+                MyFrameRateLabel.Content = _frameRate.ToString();
             }
 
             // Update the background of the canvas by converting MouseMove info to RGB info.
-            var redColor = (byte) (_pt.X/3.0);
-            var blueColor = (byte) (_pt.Y/2.0);
-            myCanvas.Background = new SolidColorBrush(Color.FromRgb(redColor, 0x0, blueColor));
+            _redColor = (byte)(_pt.X/3.0);
+            _blueColor = (byte)(_pt.Y/2.0);
+            MyCanvas.Background = new SolidColorBrush(Color.FromRgb(_redColor, 0x0, _blueColor));
         }
 
         public void MouseMoveHandler(object sender, MouseEventArgs e)
         {
             // Retreive the coordinates of the mouse button event.
-            _pt = e.GetPosition((UIElement) sender);
+            _pt = e.GetPosition((UIElement)sender);
         }
     }
 }
