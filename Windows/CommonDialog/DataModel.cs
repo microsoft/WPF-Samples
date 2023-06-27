921b2f7fc79bc1e4ea8fc6bbf16377f7b5bedf04
dipeshmsft <dipeshkumar@microsoft.com>
2023-06-27 15:40:45 +0530

    Added comments and FileOK event handling

--- Windows/CommonDialog/DataModel.cs
+++ Windows/CommonDialog/DataModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -73,6 +74,36 @@ namespace CommonDialogs
         private void OnPropertyChanged(string v)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(v));
+        }
+
+        public static DataModel GetDialogRelatedInfo(Microsoft.Win32.CommonDialog cd)
+        {
+            DataModel model = new DataModel();
+
+            if (cd is OpenFileDialog ofd)
+            {
+                model.ResultTitle = "Open File Dialog Results";
+                model.DialogDescription = "OpenFileDialog is a wrapper around the Win32 common dialog boxes.\n\nThis control allows developers to retrieve the name\\ names of file to open.";
+                model.ClickOperationDescription = "In this example, we have created an OpenFileDialog that can be used to select multiple text files.\nWe have also set the ForcePreviewPane property to true, which will display the preview pane in the dialog box.\n\nOnce the user clicks OK, we read and display the contents of the file.";
+            }
+            else if (cd is SaveFileDialog sfd)
+            {
+                model.ResultTitle = "Save File Dialog Results";
+                model.DialogDescription = "SaveFileDialog is a wrapper around the Win32 common dialog boxes.\n\nThis control allows developers to retrieve the location and filename, where the user wants to save the file.";
+                model.ClickOperationDescription = "In this example, we have created an SaveFileDialog and write a small text in the selected file.\n" +
+                    "We have set the CreatePrompt and OverwritePrompt property to true, which will display a prompt warning user that the file does not exist and will be created and if you are overwriting an already existing file respectively.\n\n" +
+                    "Once the user clicks OK, we write the contents to the selected file.";
+            }
+            //else if (cd is OpenFolderDialog fbd)
+            //{
+            //    model.ResultTitle = "Open Folder Dialog Results",
+            //    model.DialogDescription = "OpenFolderDialog is a wrapper around the Win32 common dialog boxes.\n\nThis control allows developers to retrieve\\select the path of a folder.",
+            //    model.ClickOperationDescription = "In this example, we have created an OpenFolderDialog that can be used to select multiple folders (Multiselect property).\n\n" +
+            //      "Once the user clicks OK, we print all the subdirectories in each selected folder."
+            //}
+
+            return model;
+
         }
     }
 }
--- Windows/CommonDialog/MainWindow.xaml.cs
+++ Windows/CommonDialog/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.IO;
 using Microsoft.Win32;
 using System.Diagnostics;
 using System.ComponentModel;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace CommonDialogs
 {
@@ -28,27 +29,26 @@ namespace CommonDialogs
 
         private void Button_OpenFileDialog(object sender, RoutedEventArgs e)
         {
-
+            // Instantiate the open file dialog box.
+            // Setting the following properties are optional
             OpenFileDialog openFileDialog = new OpenFileDialog()
             {
                 Title = "Select any text file...",
-                InitialDirectory = @"C:\Window",
+                InitialDirectory = @"C:\Windows",
                 Filter = "Text files (*.txt)|*.txt"
             };
 
+            // Forces the preview pane to be shown irrespective of the system settings
             //openFileDialog.ForcePreviewPane = true;
+
+            // Allows to select multiple files in the dialog
             openFileDialog.Multiselect = true;
 
-            var dialogInfos = GetDialogRelatedInfo(openFileDialog);
 
-            var data = (DataModel)DataContext;
+            string text = "";
 
-            data.ResultBody = string.Empty;
-            data.ResultTitle = dialogInfos[0];
-            data.DialogDescription = dialogInfos[1];
-            data.ClickOperationDescription = dialogInfos[2];
-
-
+            // If the user clicks OK button on the dialog, the ShowDialog() method returns true
+            // otherwise it returns false.
             if (openFileDialog.ShowDialog() == true)
             {
                 var streams = openFileDialog.OpenFiles();
@@ -58,17 +58,21 @@ namespace CommonDialogs
                     {
                         using (var reader = new System.IO.StreamReader(stream))
                         {
-                            data.ResultBody += reader.ReadToEnd();
+                            text += reader.ReadToEnd();
                         }
-                        data.ResultBody += "\n\n--- END OF FILE ---\n";
+                        text += "\n\n--- END OF FILE ---\n";
                     }
                 }
             }
             else
             {
-                data.ResultBody = "No file selected. \n Either cancel button was clicked or dialog was closed";
+                text = "No file selected. \nEither cancel button was clicked or dialog was closed";
             }
 
+            // Setting the data context of this application. Not related to dialog functionality.
+            DataModel data = DataModel.GetDialogRelatedInfo(openFileDialog);
+            data.ResultBody = text;
+            DataContext = data;
         }
 
         private void Button_SaveFileDialog(object sender, RoutedEventArgs e)
@@ -80,33 +84,48 @@ namespace CommonDialogs
                 Filter = "Text files (*.txt)|*.txt"
             };
 
+            // Setting this flag to true will prompt the user if the file is going to be created.
             saveFileDialog.CreatePrompt = true;
+
+            // Setting this flag to true will prompt the user if the file is going to be overwritten.
             saveFileDialog.OverwritePrompt = true;
+
+            // Setting this flag to true will restore the Application.CurrentDirectory.
             //saveFileDialog.RestoreDirectory = true;
 
-            var dialogInfos = GetDialogRelatedInfo(saveFileDialog);
+            // We can attach an event handler to the FileOk event of the dialog box.
+            saveFileDialog.FileOk += SaveFileDialog_FileOk;
 
-            var data = (DataModel)DataContext;
-
-            data.ResultBody = string.Empty;
-            data.ResultTitle = dialogInfos[0];
-            data.DialogDescription = dialogInfos[1];
-            data.ClickOperationDescription = dialogInfos[2];
-
-            //Debug.WriteLine(Directory.GetCurrentDirectory());
-
+            string text = "";
             if (saveFileDialog.ShowDialog() == true)
             {
                 File.WriteAllText(saveFileDialog.FileName, "Saving File Successful");
-                //Debug.WriteLine(Directory.GetCurrentDirectory());
 
-                data.ResultBody = $" Selected File Name : {saveFileDialog.FileName}\nSaved the file with a small text \" Saving File Successful \"";
+                text = $" Selected File Name : {saveFileDialog.FileName}\nSaved the file with a small text \" Saving File Successful \"";
             }
             else
             {
-                data.ResultBody = "No file selected. \n Either cancel button was clicked or dialog was closed";
+                text = "No file selected. \nEither cancel button was clicked or dialog was closed";
             }
-            //Debug.WriteLine(Directory.GetCurrentDirectory());
+
+            // Setting the data context of this application. Not related to dialog functionality.
+            DataModel data = DataModel.GetDialogRelatedInfo(saveFileDialog);
+            data.ResultBody = text;
+            DataContext = data;
+        }
+
+        private void SaveFileDialog_FileOk(object? sender, CancelEventArgs e)
+        {
+            // This can be used to perform addtional validation on the selected file.
+            if(sender is SaveFileDialog sfd)
+            {
+                string selectedFile = sfd.FileName;
+                if (selectedFile.Contains(" "))
+                {
+                    MessageBox.Show("File name cannot contain spaces. Please rename the file and try again.");
+                    e.Cancel = true;
+                }
+            }
         }
 
         private void Button_OpenFolderDialog(object sender, RoutedEventArgs e)
@@ -119,76 +138,29 @@ namespace CommonDialogs
 
             //openFolderDialog.Multiselect = true;
 
-            //var dialogInfos = GetDialogRelatedInfo(openFolderDialog);
-
-            //var data = (DataModel)DataContext;
-
-            //data.ResultBody = string.Empty;
-            //data.ResultTitle = dialogInfos[0];
-            //data.DialogDescription = dialogInfos[1];
-            //data.ClickOperationDescription = dialogInfos[2];
-
+            //string text = "";
             //if (openFolderDialog.ShowDialog() == true)
             //{
-            //    data.ResultBody = "List of all subdirectories in the selected folders : \n\n";
+            //    text = "List of all subdirectories in the selected folders : \n\n";
             //    foreach (var folder in openFolderDialog.FolderNames)
             //    {
             //        DirectoryInfo d = new DirectoryInfo(folder);
-            //        data.ResultBody += folder + "\n---\n";
+            //        text += folder + "\n---\n";
 
-            //        d.GetDirectories().ToList().ForEach(x => data.ResultBody += x.FullName + "\n");
+            //        d.GetDirectories().ToList().ForEach(x => text += x.FullName + "\n");
             //    }
             //}
             //else
             //{
-            //    data.ResultBody = "No folder selected. \n Either cancel button was clicked or dialog was closed";
+            //    text = "No folder selected. \n Either cancel button was clicked or dialog was closed";
             //}
+
+            // Setting the data context of this application. Not related to dialog functionality.
+            //DataModel data = DataModel.GetDialogRelatedInfo(openFolderDialog);
+            //data.ResultBody = text;
+            //DataContext = data;
         }
 
-        private List<string> GetDialogRelatedInfo(Microsoft.Win32.CommonDialog cd)
-        {
-            List<string> info;
-
-            if (cd is OpenFileDialog ofd)
-            {
-                info = new List<string>()
-                {
-                    "Open File Dialog Results",
-                    "OpenFileDialog is a wrapper around the Win32 common dialog boxes.\n\nThis control allows developers to retrieve the name\\names of file to open.",
-                    "In this example, we have created an OpenFileDialog that can be used to select multiple text files.\nWe have also set the ForcePreviewPane property to true, which will display the preview pane in the dialog box.\n\nOnce the user clicks OK, we read and display the contents of the file."
-                };
-            }
-            else if (cd is SaveFileDialog sfd)
-            {
-                info = new List<string>()
-                {
-                    "Save File Dialog Results",
-                    "SaveFileDialog is a wrapper around the Win32 common dialog boxes.\n\nThis control allows developers to retrieve the location and filename, where the user wants to save the file.",
-                    "In this example, we have created an SaveFileDialog and write a small text in the selected file.\n" +
-                    "We have set the CreatePrompt and OverwritePrompt property to true, which will display a prompt warning user that the file does not exist and will be created and if you are overwriting an already existing file respectively.\n\n" +
-                    "Once the user clicks OK, we write the contents to the selected file."
-                };
-            }
-            //else if (cd is OpenFolderDialog fbd)
-            //{
-            //    info = new List<string>()
-            //    {
-            //        "Open Folder Dialog Results",
-            //        "OpenFolderDialog is a wrapper around the Win32 common dialog boxes.\n\nThis control allows developers to retrieve\\select the path of a folder.",
-            //        "In this example, we have created an OpenFolderDialog that can be used to select multiple folders (Multiselect property).\n\n" +
-            //        "Once the user clicks OK, we print all the subdirectories in each selected folder."
-            //    };
-            //}
-            else
-            {
-                info = new List<string>(3);
-            }
-            return info;
-
-        }
-
-        // this is for the purpose of cleaning up the saved files after the application is closed
-        private List<string> savedFiles = new List<string>();
 
     }
 }
