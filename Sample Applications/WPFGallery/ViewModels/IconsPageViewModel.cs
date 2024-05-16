da10302731fbe71054039733177e716c0fd1ee4d
Dipesh Kumar <85861525+dipeshmsft@users.noreply.github.com>
2024-05-16 23:34:26 +0530

    Embedded icons data json in the assemmbly (#581)

--- Sample Applications/WPFGallery/ViewModels/IconsPageViewModel.cs
+++ Sample Applications/WPFGallery/ViewModels/IconsPageViewModel.cs
@@ -6,6 +6,8 @@ using System.Text.Json;
 using System.Threading.Tasks;
 using WPFGallery.Models;
 using WPFGallery.Navigation;
+using System.IO;
+using System.Reflection;
 
 namespace WPFGallery.ViewModels
 {
@@ -25,9 +27,21 @@ namespace WPFGallery.ViewModels
 
         public IconsPageViewModel()
         {
-            var jsonText = File.ReadAllText("Models/IconsData.json");
+            var jsonText = ReadIconData();
             _icons = JsonSerializer.Deserialize<List<IconData>>(jsonText);
             _selectedIcon = _icons.FirstOrDefault();
+        }
+
+        private string ReadIconData()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "WPFGallery.Models.IconsData.json";
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
         }
     }
 }
