03d059d0d3e1c80209ab857891a87796f2c8ca2b
Anjali <anjalisheel@microsoft.com>
2024-06-17 16:06:26 +0530

    Fix for "No action is performed or no text is displayed about the invoked control."

--- Sample Applications/WPFGallery/Views/Navigation/MenuPage.xaml.cs
+++ Sample Applications/WPFGallery/Views/Navigation/MenuPage.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
 using WPFGallery.ViewModels;
 
@@ -22,14 +9,23 @@ namespace WPFGallery.Views
     /// </summary>
     public partial class MenuPage : Page
     {
-    public MenuPageViewModel ViewModel { get; }
+        public MenuPageViewModel ViewModel { get; }
 
-    public MenuPage(MenuPageViewModel viewModel)
-    {
-        ViewModel = viewModel;
-        DataContext = this;
+        public MenuPage(MenuPageViewModel viewModel)
+        {
+            ViewModel = viewModel;
+            DataContext = this;
 
-        InitializeComponent();
-    }
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is MenuItem menuItem)
+            {
+                StatusMenuItem.Visibility = Visibility.Visible;
+                StatusMenuItem.Text = (menuItem.Tag != null) ?  $"You pressed {menuItem.Tag}" : $"You pressed {menuItem.Header}";
+            }
+        }
     }
 }
