6cc7a759e8dfa51abb7e49372c5ea6569f612fee
Himanshi Goyal <himgoyal@microsoft.com>
2024-09-27 19:58:39 +0530

    UI Updation

--- Sample Applications/WPFGallery/Views/Samples/UserDashboardPage.xaml.cs
+++ Sample Applications/WPFGallery/Views/Samples/UserDashboardPage.xaml.cs
@@ -57,32 +57,62 @@ namespace WPFGallery.Views
 
         private void Page_SizeChanged(object sender, SizeChangedEventArgs e)
         {
-            if(e.NewSize.Width < 800)
+            if(e.NewSize.Width < 600)
             {
-                Grid.SetColumnSpan(UserListGrid,2);
-                Grid.SetRowSpan(UserListGrid, 1);
-                Grid.SetRow(UserListGrid, 0);
-                Grid.SetColumn(UserListGrid, 0);
-                Grid.SetColumnSpan(UserDetailsGrid, 2);
-                Grid.SetRowSpan(UserDetailsGrid, 1);
-                Grid.SetRow(UserDetailsGrid, 1);
-                Grid.SetColumn(UserDetailsGrid, 0);
+                Grid_SetProperties(UserListGrid, 0, 0, 1, 2);
+                Grid_SetProperties(UserDetailsGrid, 1, 0, 1, 2);
+                UserDetailsGrid.Margin = new Thickness(0);
+                UserDetailFormGrid.Margin = new Thickness(20,10,20,10);
                 UserList.ClearValue(WidthProperty);
                 NewUserButton.HorizontalAlignment = HorizontalAlignment.Right;
+                Grid_SetProperties(FirstNamePanel, 0, 0, 2, 1);
+                Grid_SetProperties(LastNamePanel, 0, 1, 2, 1);
+                FirstNamePanel.Margin = new Thickness(0,0,10,0);
+                UserDetailHeader.Orientation = Orientation.Horizontal;
+                UserDetailHeaderPanel.HorizontalAlignment = HorizontalAlignment.Left;
+                UserDetailHeaderNameBox.HorizontalAlignment = HorizontalAlignment.Left;
+                UserDetailHeaderCompanyBox.HorizontalAlignment = HorizontalAlignment.Left;
+            }
+            else if(e.NewSize.Width >= 400 && e.NewSize.Width < 800)
+            {
+                Grid_SetProperties(UserListGrid, 0, 0, 2, 1);
+                Grid_SetProperties(UserDetailsGrid, 0, 1, 2, 1);
+                UserDetailsGrid.Margin = new Thickness(-10,0,-20,0);
+                UserDetailFormGrid.Margin = new Thickness(0,10,0,10);
+                UserList.Width = 240;
+                NewUserButton.HorizontalAlignment = HorizontalAlignment.Center;
+                Grid_SetProperties(FirstNamePanel, 0, 0, 1, 2);
+                Grid_SetProperties(LastNamePanel, 1, 0, 1, 2);
+                FirstNamePanel.Margin = new Thickness(0);
+                UserDetailHeader.Orientation = Orientation.Vertical;
+                UserDetailHeaderPanel.HorizontalAlignment = HorizontalAlignment.Center;
+                UserDetailHeaderNameBox.HorizontalAlignment = HorizontalAlignment.Center;
+                UserDetailHeaderCompanyBox.HorizontalAlignment = HorizontalAlignment.Center;
             }
             else
             {
-                Grid.SetColumnSpan(UserListGrid, 1);
-                Grid.SetRowSpan(UserListGrid, 2);
-                Grid.SetRow(UserListGrid, 0);
-                Grid.SetColumn(UserListGrid, 0);
-                Grid.SetColumnSpan(UserDetailsGrid, 1);
-                Grid.SetRowSpan(UserDetailsGrid, 2);
-                Grid.SetRow(UserDetailsGrid, 0);
-                Grid.SetColumn(UserDetailsGrid, 1);
+                Grid_SetProperties(UserListGrid, 0, 0, 2, 1);
+                Grid_SetProperties(UserDetailsGrid, 0, 1, 2, 1);
+                UserDetailsGrid.Margin = new Thickness(0);
+                UserDetailFormGrid.Margin = new Thickness(20,10,20,10);
                 UserList.Width = 300;
                 NewUserButton.HorizontalAlignment = HorizontalAlignment.Center;
+                Grid_SetProperties(FirstNamePanel, 0, 0, 2, 1);
+                Grid_SetProperties(LastNamePanel, 0, 1, 2, 1);
+                FirstNamePanel.Margin = new Thickness(0,0,10,0);
+                UserDetailHeader.Orientation = Orientation.Horizontal;
+                UserDetailHeaderPanel.HorizontalAlignment = HorizontalAlignment.Left;
+                UserDetailHeaderNameBox.HorizontalAlignment = HorizontalAlignment.Left;
+                UserDetailHeaderCompanyBox.HorizontalAlignment = HorizontalAlignment.Left;
             }
+        }
+
+        private void Grid_SetProperties(UIElement uiElement, int row, int column, int rowSpan, int columnSpan)
+        {
+            Grid.SetRow(uiElement, row);
+            Grid.SetColumn(uiElement, column);
+            Grid.SetRowSpan(uiElement, rowSpan);
+            Grid.SetColumnSpan(uiElement, columnSpan);
         }
     }
 }
