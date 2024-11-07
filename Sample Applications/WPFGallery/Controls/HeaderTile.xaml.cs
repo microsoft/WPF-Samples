3e1f29b3ba29ea946e5e04950a3e801ca9736ac1
Dipesh Kumar <dipeshkumar@microsoft.com>
2024-11-07 14:31:01 +0530

    Redesigned WPF Gallery homepage

---
+++ Sample Applications/WPFGallery/Controls/HeaderTile.xaml.cs
@@ -0,0 +1,53 @@
+﻿
+namespace WPFGallery.Controls
+{
+    /// <summary>
+    /// Interaction logic for HeaderTile.xaml
+    /// </summary>
+    public partial class HeaderTile : UserControl
+    {
+        public HeaderTile()
+        {
+            InitializeComponent();
+        }
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(string), typeof(HeaderTile), new PropertyMetadata(""));
+
+        public string Description
+        {
+            get { return (string)GetValue(DescriptionProperty); }
+            set { SetValue(DescriptionProperty, value); }
+        }
+        public static readonly DependencyProperty DescriptionProperty =
+            DependencyProperty.Register("ColorExplanation", typeof(string), typeof(HeaderTile), new PropertyMetadata(""));
+
+        public string Link
+        {
+            get { return (string)GetValue(LinkProperty); }
+            set { SetValue(LinkProperty, value); }
+        }
+
+        public static readonly DependencyProperty LinkProperty = 
+            DependencyProperty.Register("Link", typeof(string), typeof(HeaderTile), new PropertyMetadata(null));
+
+        public object Source
+        {
+            get { return (object)GetValue(SourceProperty); }
+            set { SetValue(SourceProperty, value); }
+        }
+
+        public static readonly DependencyProperty SourceProperty =
+            DependencyProperty.Register("Source", typeof(object), typeof(HeaderTile), new PropertyMetadata(null));
+
+        private void RootButton_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(Link) { UseShellExecute = true });
+        }
+    }
+}
---
+++ Sample Applications/WPFGallery/Controls/TileGallery.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WPFGallery.Controls
+{
+    /// <summary>
+    /// Interaction logic for TileGallery.xaml
+    /// </summary>
+    public partial class TileGallery : UserControl
+    {
+        public TileGallery()
+        {
+            InitializeComponent();
+        }
+
+        private void ScrollBackButton_Click(object sender, RoutedEventArgs e)
+        {
+            double newOffSet = RootScrollViewer.HorizontalOffset - 210;
+            RootScrollViewer.ScrollToHorizontalOffset(newOffSet);
+            UpdateScrollButtonsVisibility();
+        }
+
+        private void ScrollForwardButton_Click(object sender, RoutedEventArgs e)
+        {
+            double newOffSet = RootScrollViewer.HorizontalOffset + 210;
+            RootScrollViewer.ScrollToHorizontalOffset(newOffSet);
+            UpdateScrollButtonsVisibility();
+        }
+
+        private void UpdateScrollButtonsVisibility()
+        {
+            ScrollBackButton.Visibility = Visibility.Visible;
+            ScrollForwardButton.Visibility = Visibility.Visible;
+
+            if (RootScrollViewer.ActualWidth < TilesPanel.ActualWidth)
+            {
+                if(RootScrollViewer.HorizontalOffset == 0)
+                {
+                    ScrollBackButton.Visibility = Visibility.Collapsed;
+                }
+                else if(RootScrollViewer.HorizontalOffset >= RootScrollViewer.ScrollableWidth)
+                {
+                    ScrollForwardButton.Visibility = Visibility.Collapsed;
+                }
+            }
+            else
+            {
+                ScrollBackButton.Visibility = Visibility.Collapsed;
+                ScrollForwardButton.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void RootScrollViewer_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            UpdateScrollButtonsVisibility();
+        }
+    }
+}
