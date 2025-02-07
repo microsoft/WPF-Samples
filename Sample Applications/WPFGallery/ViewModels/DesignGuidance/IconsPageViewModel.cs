89ce3178ef3d416c1957c40092a8d29fa71158e9
drusteeby <drusteeby@gmail.com>
2025-02-06 20:21:42 -0500

    adding text box with search capability. Moving loading logic to relay command OnLoaded to make use of async methods.

--- Sample Applications/WPFGallery/ViewModels/DesignGuidance/IconsPageViewModel.cs
+++ Sample Applications/WPFGallery/ViewModels/DesignGuidance/IconsPageViewModel.cs
@@ -1,8 +1,8 @@
 ﻿using System.Text.Json;
 using WPFGallery.Models;
-using WPFGallery.Navigation;
 using System.IO;
-using System.Reflection;
+
+
 
 namespace WPFGallery.ViewModels
 {
@@ -15,28 +15,66 @@ namespace WPFGallery.ViewModels
         private string _pageDescription = "Guide showing how to use icons in your application.";
 
         [ObservableProperty]
-        private ICollection<IconData> _icons;
+        private ICollection<IconData> _allIcons = [];
 
         [ObservableProperty]
         private IconData? _selectedIcon;
 
-        public IconsPageViewModel()
+        [ObservableProperty]
+        private string _searchText = string.Empty;
+
+        [ObservableProperty]
+        private ObservableCollection<IconData> _searchFilteredIcons = [];
+
+        [RelayCommand]
+        private async Task LoadData()
         {
-            var jsonText = ReadIconData();
-            _icons = JsonSerializer.Deserialize<List<IconData>>(jsonText);
-            _selectedIcon = _icons.FirstOrDefault();
+            AllIcons = await ReadIconData();
+            SelectedIcon = AllIcons.FirstOrDefault();
+            SearchFilteredIcons = new ObservableCollection<IconData>(AllIcons);
         }
 
-        private string ReadIconData()
+        public IconsPageViewModel()
+        {
+            PropertyChanged += IconsPageViewModel_PropertyChanged;
+        }
+
+        private static async Task<IList<IconData>> ReadIconData()
         {
             var assembly = Assembly.GetExecutingAssembly();
             var resourceName = "WPFGallery.Models.IconsData.json";
 
-            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
-            using (StreamReader reader = new StreamReader(stream))
+            using Stream stream = assembly.GetManifestResourceStream(resourceName);
+            return await JsonSerializer.DeserializeAsync<List<IconData>>(stream);
+        }
+        private void IconsPageViewModel_PropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(e.PropertyName) && e.PropertyName.Equals(nameof(SearchText)))
             {
-                return reader.ReadToEnd();
+                var filteredItems = AllIcons.AsParallel()
+                                            .Where(icon => icon.Name.IndexOf(SearchText, StringComparison.OrdinalIgnoreCase) >= 0);
+                                            
+
+                //cache the name here to set the selected item after clearing and repopulating the list
+                var selectedIconName = SelectedIcon?.Name;
+
+                SearchFilteredIcons.Clear();
+                foreach (var item in filteredItems)
+                {
+                    SearchFilteredIcons.Add(item);
+                }
+
+                //keep the selected icon the same if it exists in the search results, if not select the first one
+                Func<IconData, bool> predicate =
+                  !string.IsNullOrWhiteSpace(selectedIconName) &&
+                  SearchFilteredIcons.Any(icon => icon.Name.Equals(selectedIconName)) ?
+                  icon => icon.Name.Equals(selectedIconName) :
+                  icon => true;
+
+                SelectedIcon = SearchFilteredIcons.FirstOrDefault(predicate);
             }
         }
+
+        
     }
 }
