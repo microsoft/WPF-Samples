4fc1ba065dd4b426b4e04161cd78f62598d3e438
Dru Steeby <drusteeby@gmail.com>
2025-02-24 12:35:33 -0500

    Adds icon searching for issue #633  (#682)
    * moving iconspageviewmodel to match folder structure of view
    
    * adding text box with search capability. Moving loading logic to relay command OnLoaded to make use of async methods.
    
    * removing unnecessary parallel, rearranging some logic
    
    * moving searchtext changed logic to OnSearchTextChanged method
    
    * fixing textbox size, adding a label

---
+++ Sample Applications/WPFGallery/ViewModels/DesignGuidance/IconsPageViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System.Text.Json;
+using WPFGallery.Models;
+using System.IO;
+
+
+
+namespace WPFGallery.ViewModels
+{
+    public partial class IconsPageViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string _pageTitle = "Icons";
+
+        [ObservableProperty]
+        private string _pageDescription = "Guide showing how to use icons in your application.";
+
+        [ObservableProperty]
+        private ICollection<IconData> _allIcons = [];
+
+        [ObservableProperty]
+        private IconData? _selectedIcon;
+
+        [ObservableProperty]
+        private string _searchText = string.Empty;
+
+        [ObservableProperty]
+        private ObservableCollection<IconData> _searchFilteredIcons = [];
+
+        [RelayCommand]
+        private async Task LoadData()
+        {
+            AllIcons = await ReadIconData();
+            SelectedIcon = AllIcons.FirstOrDefault();
+            SearchFilteredIcons = new ObservableCollection<IconData>(AllIcons);
+        }
+
+        private static async Task<IList<IconData>> ReadIconData()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "WPFGallery.Models.IconsData.json";
+
+            using Stream stream = assembly.GetManifestResourceStream(resourceName);
+            return await JsonSerializer.DeserializeAsync<List<IconData>>(stream);
+        }
+
+        partial void OnSearchTextChanged(string searchText)
+        {
+            //cache the name here to set the selected item after clearing and repopulating the list
+            var selectedIconName = SelectedIcon?.Name;
+            SearchFilteredIcons.Clear();
+
+            var searchFilteredIconData = AllIcons.Where(icon => icon.Name.IndexOf(SearchText, StringComparison.OrdinalIgnoreCase) >= 0);
+            foreach (var item in searchFilteredIconData)
+            {
+                SearchFilteredIcons.Add(item);
+            }
+
+            //keep the selected icon the same if it exists in the search results, if not select the first one
+            Func<IconData, bool> predicate =
+              !string.IsNullOrWhiteSpace(selectedIconName) &&
+              SearchFilteredIcons.Any(icon => icon.Name.Equals(selectedIconName)) ?
+              icon => icon.Name.Equals(selectedIconName) :
+              icon => true;
+
+            SelectedIcon = SearchFilteredIcons.FirstOrDefault(predicate);
+        }   
+    }
+}
