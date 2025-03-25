1e58a05bf42a365bb5c75e7c50cc4f43c85e12dd
Sia Gupta <siagupta@microsoft.com>
2025-03-25 11:09:50 +0530

    Adding class for ResizeGrip and its ViewModel

---
+++ Sample Applications/WPFGallery/ViewModels/ResizeGripPageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFGallery.ViewModels
+{
+    public partial class GroupBoxPageViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string _pageTitle = "Hyperlink";
+
+        [ObservableProperty]
+        private string _pageDescription = "";
+
+        public GroupBoxPageViewModel() { }
+    }
+}
