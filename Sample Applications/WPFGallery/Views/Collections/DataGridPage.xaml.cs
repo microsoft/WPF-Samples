f5a69cf75ca19d3ffb409bcbb558710dff347ea6
dipeshmsft <dipeshkumar@microsoft.com>
2025-09-26 01:13:43 +0530

    Fixed DataGrid component backgrounds discrepancy in HC desert mode.

--- Sample Applications/WPFGallery/Views/Collections/DataGridPage.xaml.cs
+++ Sample Applications/WPFGallery/Views/Collections/DataGridPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Documents;
+﻿using Microsoft.Win32;
+using System.Windows.Documents;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
@@ -17,6 +18,32 @@ namespace WPFGallery.Views
             ViewModel = viewModel;
             DataContext = this;
             InitializeComponent();
+
+            SystemEvents.UserPreferenceChanged += SystemEvents_UserPreferenceChanged;
+            this.Loaded += (s, e) => UpdatePageVisuals();
+
+        }
+
+        private void SystemEvents_UserPreferenceChanged(object sender, UserPreferenceChangedEventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                UpdatePageVisuals();
+            });
+        }
+
+        private void UpdatePageVisuals()
+        {
+            if(SystemParameters.HighContrast == true)
+            {
+                SampleDataGrid.SetResourceReference(BackgroundProperty, SystemColors.ControlBrushKey);
+                SampleDataGrid.SetResourceReference(ForegroundProperty, SystemColors.ControlTextBrushKey);
+            }
+            else
+            {
+                SampleDataGrid.SetResourceReference(BackgroundProperty, DependencyProperty.UnsetValue);
+                SampleDataGrid.SetResourceReference(ForegroundProperty, "TextFillColorPrimaryBrush");
+            }
         }
     }
 }
