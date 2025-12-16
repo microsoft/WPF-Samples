56d31cfae038864cfd09abdb779190e591df1276
Dipesh Kumar <dipeshkumar@microsoft.com>
2025-12-16 14:50:40 +0530

    Added MessageBox samples

---
+++ Sample Applications/WPFGallery/ViewModels/System/MessageBoxPageViewModel.cs
@@ -0,0 +1,125 @@
+// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
+// Copyright (C) Leszek Pomianowski and WPF UI Contributors.
+// All Rights Reserved.
+
+namespace WPFGallery.ViewModels;
+
+/// <summary>
+/// Interaction logic for MessageBoxPage.xaml
+/// </summary>
+public partial class MessageBoxPageViewModel : ObservableObject 
+{
+	[ObservableProperty]
+	private string _pageTitle = "MessageBox";
+
+	[ObservableProperty]
+	private string _pageDescription = "";
+
+    [ObservableProperty]
+    private string _defaultMessageResult = "No message shown yet";
+
+    [ObservableProperty]
+    private string _customTitleResult = "No message shown yet";
+
+    [ObservableProperty]
+    private int _selectedButtonIndex = 0;
+
+    [ObservableProperty]
+    private string _differentButtonsResult = "No button clicked yet";
+
+    [ObservableProperty]
+    private string _differentButtonsXamlCode = "<Button Content=\"Show MessageBox\" Click=\"ShowMessageBoxButton_Click\" />";
+
+    [ObservableProperty]
+    private string _differentButtonsCSharpCode = string.Format(_differentButtonsMessageBoxSampleCSharpCodeString, "\tMessageBox.Show(\"Message\", \"Title\", MessageBoxButton.OK);");
+
+    partial void OnSelectedButtonIndexChanged(int value)
+    {
+        UpdateButtonCodeSnippets(value);
+    }
+
+    private void UpdateButtonCodeSnippets(int index)
+    {
+        string content = (MessageBoxButton)index switch
+        {
+            MessageBoxButton.OK => "\tMessageBox.Show(\"Message\", \"Title\", MessageBoxButton.OK);",
+            MessageBoxButton.OKCancel => "\tvar result = MessageBox.Show(\"Message\", \"Title\", MessageBoxButton.OKCancel);\n\tif (result == MessageBoxResult.OK)\n\t{\n\t    // User clicked OK\n\t}",
+            MessageBoxButton.AbortRetryIgnore => "\tvar result = MessageBox.Show(\"Message\", \"Title\", MessageBoxButton.AbortRetryIgnore);\n\tif (result == MessageBoxResult.Abort)\n\t{\n\t    // User clicked Abort\n\t}\n\telse if (result == MessageBoxResult.Retry)\n\t{\n\t    // User clicked Retry\n\t}\n\telse if (result == MessageBoxResult.Ignore)\n\t{\n\t    // User clicked Ignore\n\t}",
+            MessageBoxButton.YesNoCancel => "\tvar result = MessageBox.Show(\"Message\", \"Title\", MessageBoxButton.YesNoCancel);\n\tif (result == MessageBoxResult.Yes)\n\t{\n\t    // User clicked Yes\n\t}\n\telse if (result == MessageBoxResult.No)\n\t{\n\t    // User clicked No\n\t}",
+            MessageBoxButton.YesNo => "\tvar result = MessageBox.Show(\"Message\", \"Title\", MessageBoxButton.YesNo);\n\tif (result == MessageBoxResult.Yes)\n\t{\n\t    // User clicked Yes\n\t}\n\telse if (result == MessageBoxResult.No)\n\t{\n\t    // User clicked No\n\t}",
+            MessageBoxButton.RetryCancel => "\tvar result = MessageBox.Show(\"Message\", \"Title\", MessageBoxButton.RetryCancel);\n\tif (result == MessageBoxResult.Retry)\n\t{\n\t    // User clicked Retry\n\t}",
+            MessageBoxButton.CancelTryContinue => "\tvar result = MessageBox.Show(\"Message\", \"Title\", MessageBoxButton.CancelTryContinue);\n\tif (result == MessageBoxResult.TryAgain)\n\t{\n\t    // User clicked Try Again\n\t}\n\telse if (result == MessageBoxResult.Continue)\n\t{\n\t    // User clicked Continue\n\t}",
+            _ => "\tMessageBox.Show(\"Message\", \"Title\", MessageBoxButton.OK);"
+        };
+
+        DifferentButtonsCSharpCode = string.Format(_differentButtonsMessageBoxSampleCSharpCodeString, content);
+    }
+
+    [ObservableProperty]
+    private int _selectedImageIndex = 0;
+
+    [ObservableProperty]
+    private string _differentImagesResult = "No image example shown yet";
+
+    [ObservableProperty]
+    private string _differentImagesXamlCode = "<Button Content=\"Show MessageBox\" Click=\"ShowMessageButton_Click\" />";
+
+    [ObservableProperty]
+    private string _differentImagesCSharpCode = string.Format(_differentImagesMessageBoxSampleCSharpCodeString, "\tMessageBox.Show(\"Message\", \"Title\", MessageBoxButton.OK, MessageBoxImage.None);");
+
+    partial void OnSelectedImageIndexChanged(int value)
+    {
+        UpdateImageCodeSnippets(value);
+    }
+
+    private void UpdateImageCodeSnippets(int index)
+    {
+        string content = (MessageBoxImage)index switch
+        {
+            MessageBoxImage.None => "\tMessageBox.Show(\"Message\", \"Title\", MessageBoxButton.OK, MessageBoxImage.None);",
+            MessageBoxImage.Error => "\t// MessageBoxImage.Error (also Hand, Stop)\n\tMessageBox.Show(\"An error occurred!\", \"Error\", MessageBoxButton.OK, MessageBoxImage.Error);",
+            MessageBoxImage.Question => "\t// MessageBoxImage.Question\n\tvar result = MessageBox.Show(\"Do you want to continue?\", \"Question\", MessageBoxButton.YesNo, MessageBoxImage.Question);",
+            MessageBoxImage.Warning => "\t// MessageBoxImage.Warning (also Exclamation)\n\tMessageBox.Show(\"Warning: This action may have consequences.\", \"Warning\", MessageBoxButton.OKCancel, MessageBoxImage.Warning);",
+            MessageBoxImage.Information => "\t// MessageBoxImage.Information (also Asterisk)\n\tMessageBox.Show(\"Operation completed successfully.\", \"Information\", MessageBoxButton.OK, MessageBoxImage.Information);",
+            _ => "\tMessageBox.Show(\"Message\", \"Title\", MessageBoxButton.OK, MessageBoxImage.None);"
+        };
+
+        DifferentImagesCSharpCode = string.Format(_differentImagesMessageBoxSampleCSharpCodeString, content);
+    }
+
+    [ObservableProperty]
+    private string _commonMessagesResult = "No common message shown yet";
+
+    [ObservableProperty]
+    private string _commonMessagesXamlCode = @"<WrapPanel Margin=""0,0,0,10"">
+    <Button Content=""Information"" Click=""ShowInformationButton_Click"" />
+    <Button Content=""Error"" Click=""ShowErrorButton_Click"" />
+    <Button Content=""Warning"" Click=""ShowWarningButton_Click"" />
+</WrapPanel>";
+
+    [ObservableProperty]
+    private string _commonMessagesCSharpCode = @"// Information
+private void ShowInformationButton_Click(object sender, RoutedEventArgs e)
+{
+    MessageBox.Show(""Operation completed successfully."", ""Information"", MessageBoxButton.OK, MessageBoxImage.Information);
+}
+
+// Error
+private void ShowErrorButton_Click(object sender, RoutedEventArgs e)
+{
+    MessageBox.Show(""An error occurred!"", ""Error"", MessageBoxButton.OK, MessageBoxImage.Error);
+}
+
+// Warning
+private void ShowWarningButton_Click(object sender, RoutedEventArgs e)
+{
+    MessageBox.Show(""This action cannot be undone!"", ""Warning"", MessageBoxButton.OKCancel, MessageBoxImage.Warning);
+}";
+
+    [ObservableProperty]
+    private string _customDefaultResult = "No selection made";
+
+    private const string _differentButtonsMessageBoxSampleCSharpCodeString = "private void ShowMessageBoxButton_Click(object sender, RoutedEventArgs e)\n{{\n{0}\n}}";                
+    private const string _differentImagesMessageBoxSampleCSharpCodeString = "private void ShowMessageBoxButton_Click(object sender, RoutedEventArgs e)\n{{\n{0}\n}}";
+}
---
+++ Sample Applications/WPFGallery/Views/System/MessageBoxPage.xaml.cs
@@ -0,0 +1,115 @@
+using System.Windows.Documents;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using WPFGallery.ViewModels;
+
+namespace WPFGallery.Views
+{
+    /// <summary>
+    /// Interaction logic for MessageBoxPage.xaml
+    /// </summary>
+    public partial class MessageBoxPage : Page
+    {
+        public MessageBoxPageViewModel ViewModel { get; }
+
+        public MessageBoxPage(MessageBoxPageViewModel viewModel)
+        {
+            ViewModel = viewModel;
+            DataContext = this;
+            InitializeComponent();
+        }
+
+        // 1. Default MessageBox
+        private void ShowDefaultMessageButton_Click(object sender, RoutedEventArgs e)
+        {
+            var result = MessageBox.Show("This is a simple message box!");
+            ViewModel.DefaultMessageResult = $"Result: {result}";
+        }
+
+        // 2. Custom Title and Description
+        private void ShowCustomTitleButton_Click(object sender, RoutedEventArgs e)
+        {
+            var result = MessageBox.Show("This is a detailed description of what happened or what action is needed.", "Custom Title");
+            ViewModel.CustomTitleResult = $"Result: {result}";
+        }
+
+        // 3. Different Buttons - Using ComboBox
+        private void ShowButtonFromComboBox_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxButton buttonType = ViewModel.SelectedButtonIndex switch
+            {
+                0 => MessageBoxButton.OK,
+                1 => MessageBoxButton.OKCancel,
+                2 => MessageBoxButton.YesNo,
+                3 => MessageBoxButton.YesNoCancel,
+                _ => MessageBoxButton.OK
+            };
+
+            string buttonName = ViewModel.SelectedButtonIndex switch
+            {
+                0 => "OK",
+                1 => "OK/Cancel",
+                2 => "Yes/No",
+                3 => "Yes/No/Cancel",
+                _ => "OK"
+            };
+
+            var result = MessageBox.Show($"This MessageBox has {buttonName} button(s).", $"{buttonName} Button(s)", buttonType);
+            ViewModel.DifferentButtonsResult = $"Buttons: {buttonName} | Result: {result}";
+        }
+
+        // 4. Different Images - Using ComboBox
+        private void ShowImageFromComboBox_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxImage imageType = ViewModel.SelectedImageIndex switch
+            {
+                0 => MessageBoxImage.None,
+                1 => MessageBoxImage.Error,
+                2 => MessageBoxImage.Question,
+                3 => MessageBoxImage.Warning,
+                4 => MessageBoxImage.Information,
+                _ => MessageBoxImage.None
+            };
+
+            string imageName = ViewModel.SelectedImageIndex switch
+            {
+                0 => "None",
+                1 => "Error",
+                2 => "Question",
+                3 => "Warning",
+                4 => "Information",
+                _ => "None"
+            };
+
+            var result = MessageBox.Show($"This MessageBox displays the {imageName} icon.", $"{imageName} Icon", MessageBoxButton.OK, imageType);
+            ViewModel.DifferentImagesResult = $"Image: {imageName} | Result: {result}";
+        }
+
+        // 6. Common Messages (Information, Error, Warning)
+        private void ShowCommonInformation_Click(object sender, RoutedEventArgs e)
+        {
+            var result = MessageBox.Show("The operation completed successfully.", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+            ViewModel.CommonMessagesResult = $"Type: Information | Result: {result}";
+        }
+
+        private void ShowCommonError_Click(object sender, RoutedEventArgs e)
+        {
+            var result = MessageBox.Show("An error occurred! The operation could not be completed.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            ViewModel.CommonMessagesResult = $"Type: Error | Result: {result}";
+        }
+
+        private void ShowCommonWarning_Click(object sender, RoutedEventArgs e)
+        {
+            var result = MessageBox.Show("This action cannot be undone! Do you want to continue?", "Warning", MessageBoxButton.OKCancel, MessageBoxImage.Warning);
+            ViewModel.CommonMessagesResult = $"Type: Warning | Result: {result}";
+        }
+
+        // 7. Custom Default Button
+        private void ShowCustomDefaultButton_Click(object sender, RoutedEventArgs e)
+        {
+            var result = MessageBox.Show("Do you want to save changes? Press Enter to select the default 'No' button.", "Save Changes", MessageBoxButton.YesNoCancel, MessageBoxImage.Question, MessageBoxResult.No);
+            ViewModel.CustomDefaultResult = $"User selected: {result}";
+        }
+    }
+}
